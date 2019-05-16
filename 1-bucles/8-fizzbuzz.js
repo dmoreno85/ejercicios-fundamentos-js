793679b96f6eb2be1416ff7e73a49ead295e2f76
David <dmoreno788@gmail.com>
2019-05-16 13:49:06 +0200

    8th exercise done

--- 1-bucles/8-fizzbuzz.js
+++ 1-bucles/8-fizzbuzz.js
@@ -3,3 +3,23 @@
 // pero que los múltiplos de 3 imprima GEEKS en lugar del numero
 // y los múltiplos de 5 imprima HUBS.
 // Además los múltiplos de 3 y 5 ha de imprimir GEEKSHUBS
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) console.log('GEEKS')
+
+    else if (i % 5 == 0) console.log('HUBS')
+
+    if ((i % 5 == 0 )&&(i % 3 == 0)) console.log("GEEKSHUBS") //comparten multiplo
+
+    else console.log(i)
+}; 
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0) console.log('GEEKS')
+
+//     else if (i % 5 == 0) console.log('HUBS')
+
+//     if ((i % 5 == 0 )&&(i % 3 == 0)) console.log("GEEKSHUBS") //Imprime en cada multiplo de 3 o de 5
+
+//     else console.log(i)
+// }; 
